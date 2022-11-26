670619db60b412a941ca2d05bab45cf90a0567c3
DENIS <hindoo@mail.ru>
2022-11-26 18:34:37 +0300

    first exercise

---
+++ exercises/seminar6_41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+ void evenCounter ()
+
+     {
+
+        string inp = string.Empty;
+
+        float readings;
+
+        int counter = 0;
+
+
+            while ( inp != "stop")
+            {
+            Console.WriteLine("введите число или \"stop\" для выхода:");
+            inp =  Console.ReadLine();
+
+                if (inp == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    bool check = float.TryParse(inp, out readings);
+
+                    if (check && readings > 0 )
+                    {
+                        counter++;
+                    }
+                    
+                }
+
+
+            }
+
+            Console.WriteLine($"количество введенных положительных чисел равно {counter}");
+
+         
+        }
+
+    evenCounter ();
+
+   
+    
